602d62a83b075dc362d70719b5488ef5740edd76
Szymon Kraus <szymon.kraus@hemmersbach.com>
2020-07-24 15:22:31 +0200

    configured koin.

--- app/src/main/java/com/mobeedev/employees/EmployeesApplication.kt
+++ app/src/main/java/com/mobeedev/employees/EmployeesApplication.kt
@@ -1,4 +1,23 @@
 package com.mobeedev.employees
 
-class EmployeesApplication {
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class EmployeesApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initializeKoin()
+    }
+
+    private fun initializeKoin() =
+        startKoin {
+            androidContext(this@EmployeesApplication)
+            androidLogger()
+            //todo load base koin modules when defined [SK]
+        }
+
 }
